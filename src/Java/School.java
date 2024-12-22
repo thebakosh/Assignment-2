952a30ddd51b6e3e7e931d65f53546943f7e4952
Kuanysh Baktiyar <baktiar.kuan@gmail.com>
2024-12-23 01:19:18 +0500

    Task 3

---
+++ src/Java/School.java
@@ -0,0 +1,25 @@
+package Java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class School {
+    private List<Person> members;
+
+    public School() {
+        members = new ArrayList<>();
+    }
+
+    public void addMember(Person person) {
+        members.add(person);
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        for (Person member : members) {
+            result += member.toString() + "\n";
+        }
+        return result;
+    }
+}
