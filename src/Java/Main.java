3a5dac027c0883cefc27cdcb05530014bc5440ed
Kuanysh Baktiyar <baktiar.kuan@gmail.com>
2024-12-22 22:51:19 +0500

    Task 1

---
+++ src/Java/Main.java
@@ -0,0 +1,14 @@
+package Java;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Baktiyar", "Kuanysh",17, true);
+        System.out.println(person);
+    }
+
+}
+
+
