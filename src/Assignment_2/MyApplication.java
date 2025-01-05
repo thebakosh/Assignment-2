787494a0d066961787a03c26557eae7577fa53e3
Kuanysh Baktiyar <baktiar.kuan@gmail.com>
2025-01-06 00:06:19 +0500

    .

--- src/Assignment_2/MyApplication.java
+++ src/Assignment_2/MyApplication.java
@@ -7,10 +7,10 @@ import java.util.List;
 public class MyApplication {
 
     public static void main(String[] args) {
-        Employee employee1 = new Employee("John", "Lennon", "Manager", 27045.78);
-        Employee employee2 = new Employee("George", "Harrison", "Developer", 50000.00);
-        Student student1 = new Student("Ringo", "Starr", 2.5);
-        Student student2 = new Student("Paul", "McCartney", 3.8);
+        Employee employee1 = new Employee("Akniet", "Balyk", "Manager", 27045.78);
+        Employee employee2 = new Employee("Alim", "Aldiyarov", "Developer", 50000.00);
+        Student student1 = new Student("Baktiyar", "Kuanysh", 3.44);
+        Student student2 = new Student("Ermakhan", "Khamitov", 2.88);
 
         List<Person> people = new ArrayList<>();
         people.add(employee1);
